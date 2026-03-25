698c0e1ab20941b71d9c1eda8a50a446939dc01a
Jie Wang <psxjw19@nottingham.ac.uk>
2026-03-25 14:11:42 +0800

    feat: card scroll sync with indicators, time range filter

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,32 @@
 // home.js
 Page({
-  onLoad() {},
+  data: {
+    currentIndex: 0,
+    timeRange: 'day',
+    cardWidth: 600,
+    cardGap: 20
+  },
+
+  onLoad() {
+    const cardWidth = 600
+    const cardGap = 20
+    this.setData({ cardWidth, cardGap })
+  },
+
+  onScroll(e) {
+    const scrollLeft = e.detail.scrollLeft
+    const cardWidth = this.data.cardWidth
+    const cardGap = this.data.cardGap
+    const totalWidth = cardWidth + cardGap
+    const currentIndex = Math.round(scrollLeft / totalWidth)
+    if (currentIndex !== this.data.currentIndex) {
+      this.setData({ currentIndex })
+    }
+  },
+
+  setTimeRange(e) {
+    this.setData({ timeRange: e.currentTarget.dataset.range })
+  },
 
   editCard() {
     wx.navigateTo({ url: '/pages/edit/edit' })
